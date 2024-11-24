eaf97051fc1bfb3817dcdebb9ebdb40889729924
RisheendraReddy <RisheendraReddy@users.noreply.github.com>
2024-11-24 12:23:14 -0500

    Updates...

--- backend/server.py
+++ backend/server.py
@@ -66,4 +66,4 @@ def add_question():
         abort(500, description="Failed to save questions data.")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
