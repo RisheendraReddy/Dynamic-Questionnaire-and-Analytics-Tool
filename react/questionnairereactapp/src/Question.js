85d3c6337cc1615ec9abc5722ae7c152bb9834a7
RisheendraReddy <RisheendraReddy@users.noreply.github.com>
2025-03-27 08:07:51 -0700

    Update Question.js

--- react/questionnairereactapp/src/Question.js
+++ react/questionnairereactapp/src/Question.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import ProgressIndicator from './ProgressIndicator';
 
-function Question({ question, onAnswer }) {
+function Question({ question, onAnswer, currentQuestion, totalQuestions }) {
   const handleResponse = (score) => {
     onAnswer(question.category, score);
   };
 
   return (
     <div className="question-container">
+        {/* Add the progress indicator */}
+      <ProgressIndicator
+        currentQuestion={currentQuestion}
+        totalQuestions={totalQuestions}
+      />
         <h2>Category: {question.category}</h2>
       <h3>{question.text}</h3>
       {/* Replace these buttons with inputs if you want more complex responses */}
